ad026ff7e8cb2c3423ff90d0f9260a68765ae696
Daniil Gurski <daniilgurski6@gmail.com>
2026-01-28 22:08:33 +0100

    create RoomList component for better demo

--- frontend/src/pages/FilterDemo.tsx
+++ frontend/src/pages/FilterDemo.tsx
@@ -1,40 +1,12 @@
 import RoomFilter from "@/components/RoomFilter";
-import { getAllMockRooms } from "@/services/api";
-import { filterValueAtom } from "@/lib/atoms";
-import { useQuery } from "@tanstack/react-query";
-import { useAtomValue } from "jotai";
+import RoomList from "@/components/RoomList";
 
 export default function FilterDemo() {
-    const {
-        data: rooms,
-        isPending,
-        isError,
-    } = useQuery({
-        queryFn: getAllMockRooms,
-        queryKey: ["rooms"],
-    });
-    const filterValue = useAtomValue(filterValueAtom);
-
-    const availableRooms = rooms?.filter((room) => room.availability);
-    const displayedRooms = filterValue === "all" ? rooms : availableRooms;
-
-    if (isPending) {
-        return <div> Loading ... </div>;
-    }
-
-    if (isError) {
-        return <div> Error fetching rooms </div>;
-    }
-
     return (
         <div>
-            <h1> Filter rooms demo </h1>
+            <h1> FilterRoom Component Demo </h1>
             <RoomFilter />
-            <ul>
-                {displayedRooms?.map((room, index) => (
-                    <li key={index}> Room {room.room_number} </li>
-                ))}
-            </ul>
+            <RoomList />
         </div>
     );
 }
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,6 +4,7 @@ import { type Room } from "@/lib/atoms";
 export const getAllMockRooms = async () => {
     try {
         await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate a 1-second delay
+
         const response = await axios.get("/data/rooms.json");
         return response.data as Room[];
     } catch (error) {
