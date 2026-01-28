cda8f8711c14e369dffe83b451116f9853a4be8f
Daniil Gurski <daniilgurski6@gmail.com>
2026-01-28 19:34:33 +0100

    config alias imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router";
-import { router } from "./router.tsx";
-import "./style.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router";
+import { router } from "@/router";
+import "@/style.css";
 
 const client = new QueryClient();
 
