e1e2a2a49f85f82e90f66c08fb9f00d1cb0a0dc1
Johannes Flodin <jannis020609@gmail.com>
2026-01-28 16:52:43 +0100

    changed naming to ChasRooms instead to lessen on errors

---
+++ ChasRooms.Server/Domain/Entities/Room.cs
@@ -0,0 +1,12 @@
+﻿namespace ChasRooms.Server.Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; } = null!;
+        public int Capacity { get; set; }
+        public string features { get; set; } = null!;
+
+        ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+    }
+}
