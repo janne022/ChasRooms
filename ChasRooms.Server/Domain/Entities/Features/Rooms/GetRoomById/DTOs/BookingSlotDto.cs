d4222c7d4e99f2d8a39b6bc66b3eca1260269c47
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2026-01-29 20:04:38 +0100

    feat: add endpoint for getting specs on a room through id

---
+++ ChasRooms.Server/Domain/Entities/Features/Rooms/GetRoomById/DTOs/BookingSlotDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ChasRooms.Server.Domain.Entities.Features.Rooms.GetRoomById.DTOs
+{
+    public class BookingSlotDto
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
---
+++ ChasRooms.Server/Domain/Entities/Features/Rooms/GetRoomById/DTOs/GetRoomByIdResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace ChasRooms.Server.Domain.Entities.Features.Rooms.GetRoomById.DTOs
+{
+    public class GetRoomByIdResponse
+    {
+        public RoomDetailsDto Room { get; set; } = null!;
+    }
+}
