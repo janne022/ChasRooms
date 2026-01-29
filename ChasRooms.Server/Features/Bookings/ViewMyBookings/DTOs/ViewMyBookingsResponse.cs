490251c3c11cf48b1113243ca5680b992748486e
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2026-01-29 14:37:11 +0100

    Add bookingDto and response for get my bookings endpoint

---
+++ ChasRooms.Server/Features/Bookings/ViewMyBookings/DTOs/ViewMyBookingsResponse.cs
@@ -0,0 +1,10 @@
+﻿using ChasRooms.Server.Domain.DTOs.Bookings;
+using ChasRooms.Server.Domain.Entities;
+
+namespace ChasRooms.Server.Features.Bookings.ViewMyBookings.DTOs
+{
+    public class ViewMyBookingsResponse
+    {
+        public List<BookingDto> Bookings { get; set; } = new List<BookingDto>();
+    }
+}
