facb0cce01efb5a475c039f14ccb07faadf7d108
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2026-01-29 10:48:13 +0100

    Add response/request DTOs for create booking

---
+++ ChasRooms.Server/Features/Bookings/CreateBooking/DTOs/CreateBookingRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ChasRooms.Server.Features.Bookings.CreateBooking.DTOs
+{
+    public class CreateBookingRequest
+    {
+        public int RoomId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
