95ccf028d7f6063f4edac0e4acba2e154ca624ef
Daniil Gurski <daniilgurski6@gmail.com>
2026-01-30 15:13:25 +0100

    add backdrop to building map modal

--- frontend/src/components/BuildingMapModal.tsx
+++ frontend/src/components/BuildingMapModal.tsx
@@ -20,8 +20,10 @@ export default function BuildingMapModal() {
             onClose={() => setIsBuildingMapOpen(false)}
             className="relative z-50"
         >
+            <div className="fixed inset-0 bg-black/30" aria-hidden="true"></div>{" "}
+            {/* backdrop */}
             <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-                <DialogPanel className="grid max-w-3xl gap-y-2 border bg-white p-12">
+                <DialogPanel className="grid max-w-3xl gap-y-2 rounded-xl border border-gray-200 bg-white p-12">
                     <div className="flex items-center justify-between">
                         <DialogTitle className="font-bold">
                             Chas Academy
